3278d17030f607033f23760e90e78424c4f0e8db
Felipe Fernandes <felipe.fernandes.lima@usp.br>
2021-04-11 15:50:44 -0300

    Cabeçalho gerado por JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// Executa no carregamento da página
+(function () {
+  //Carregamento do cabeçalho
+  const header = document.getElementById("header");
+  header.innerHTML = buildHeader();
+})();
+
 function dropDown() {
   var x = document.getElementById("dropdown-content");
   if (x.className.indexOf("dropdown-show") == -1) { 
@@ -20,4 +27,32 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "block";
     }
   });
+}
+
+window.onscroll = function() {
+  var winScroll = document.documentElement.scrollTop;
+  if (winScroll != 0)
+      $("#h").addClass("scrolled");
+  else
+      $("#h").removeClass("scrolled");
+}
+
+function buildHeader() {
+  return (
+    '<link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet"/>' +
+    '<link rel="stylesheet" type="text/css" href="/css/header.css"/>' +
+    '<div id="h">' +
+      '<div id="h-content">' +
+          '<ul id="nav">' +
+              '<img id="logo" src="/images/logo-fundo-azul.svg"/>' +
+              '<li><h1 id="h-title">RATERIA</h1></li>' +
+              '<li><a href="http://localhost:5500/base.html">Página Inicial</a></li>' +
+              '<li><a href="http://localhost:5500/quem_somos">Quem somos</a></li>' +
+              '<li><a href="http://localhost:5500/apresentacoes">apresentações</a></li>' +
+              '<li><a href="">Nossos serviços</a></li>' +
+              '<li><a href="">Fale conosco</a></li>' +
+          '</ul>' +
+      '</div>' +
+    '</div>'
+  );
 }
