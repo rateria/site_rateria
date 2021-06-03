7d760f2798b14417edd661a806b7295e6a94fea4
Felipe Fernandes <felipe.fernandes.lima@usp.br>
2021-06-02 23:22:11 -0300

    Primeiros ajustes para integração de história

--- js/scripts.js
+++ js/scripts.js
@@ -68,7 +68,7 @@ function buildHeader() {
           '<div id="nav-button" onclick="toggleNavMenu()"></div>' +
           '<ul id="nav">' +
             '<a href="http://' + domain + '/"><li class="nav-option">Página Inicial</li></a>' +
-            '<a href="http://' + domain + '/quem_somos"><li class="nav-option">Quem somos</li></a>' +
+            '<a href="http://' + domain + '/historia"><li class="nav-option">Quem somos</li></a>' +
             '<a href="http://' + domain + '/apresentacoes"><li class="nav-option">apresentações</li></a>' +
             '<a href="http://' + domain + '/servicos"><li class="nav-option">Nossos serviços</li></a>' +
             '<a href="http://' + domain + '/contato"><li class="nav-option">Fale conosco</li></a>' +
